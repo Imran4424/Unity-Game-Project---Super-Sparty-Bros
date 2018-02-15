57a4497929a86f5abaaa5fc47c8215f42c7d7ded
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-02-15 21:19:51 +0600

    Multiplatform game super-sparty bros

---
+++ Assets/Editor/DeleteAllPlayerPrefs.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+ //this is needed since this script references the unity Editor
+
+//this is a script that enhances unity editor(aka, Editor Script)
+public class DeleteAllPlayerPrefs : ScriptableObject
+{
+
+
+	//Delete All PlayerPrefs after the confirmation dialog
+	[MenuItem ("Editor/Delete Player Prefs")]
+	static void DeletePrefs ()
+	{
+		if (EditorUtility.DisplayDialog ("Delete all player preferences?",
+			    "Are you sure you want to delete all the player preferences,this can not be undone?",
+			    "Yes", "No")) {
+
+
+			PlayerPrefs.DeleteAll ();
+		}
+
+	}
+
+
+
+}
---
+++ Assets/Editor/GameManagerEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; //this is needed since this script references the unity Editor
+
+[CustomEditor(typeof(GameManager))]
+public class GameManagerEditor : Editor { //extends the editor class
+
+
+	//called when Unity Editor Inspector is updated
+	public override void OnInspectorGUI()
+	{
+		//show the default inspector stuff for this component
+		DrawDefaultInspector();
+
+		//get a reference to the GameManager script on this target game Object
+		GameManager myGM = (GameManager) target;
+
+		//add a custom button to the Inspector component
+		if (GUILayout.Button ("Reset Player State")) 
+		{
+			//if this button pressed, then call function on script
+			PlayerPrefManager.ResetPlayerState(myGM.startLives,false);
+		}
+
+		//add a custom button to the inspector component
+		if (GUILayout.Button ("Reset Highscore"))
+		{
+			//if this button pressed , then call function on script
+			PlayerPrefManager.SetHighscore(0);
+		}
+
+		//add a custom button to inspector component
+		if (GUILayout.Button ("Output Player State")) 
+		{
+
+			//if this button pressed , then call the function on script
+			PlayerPrefManager.ShowPlayerPrefs();
+		}
+
+	}
+		
+}
